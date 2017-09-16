fd1480a1886e6576c5d3d29b0c2cb5e857b8e78f
renojackson922 <renojackson922@gmail.com>
2017-09-17 02:29:19 +0900

    minor change
    minor change

--- src/com/codenator/publicString/PublicString.java
+++ src/com/codenator/publicString/PublicString.java
@@ -5,14 +5,14 @@
 package com.codenator.publicString;
 
 public class PublicString {
-    public static final String USERNAME="test";
+    public static final String USERNAME="ubuntu";
     public static final String PASSWORD="a";
     public static final String SHELLSESSION="shellSession";
     public static final String PRINTWRITER="printWriter";
     public static final String CONNECTION="connection";
-    public static final String SOURCEFILEPUBLICPATH="/home/test/codenator";
+    public static final String SOURCEFILEPUBLICPATH="/home/ubuntu/codenator";
     public static final String STDOUT="stdout";
     public static final String STDERR="stderr";
     public static final String WEBSOCKET_ADDRESS="ws://localhost:8080/codenator/websocket";
-    public static final String SSH_SERVER_ADDRESS="192.168.37.131";
+    public static final String SSH_SERVER_ADDRESS="13.124.248.63";
 }
